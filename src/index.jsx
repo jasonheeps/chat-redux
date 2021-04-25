60aedde023ca3cd70a1e077c8dcaadc7a2ce0f37
Philipp Schaefer <pschaefer@posteo.net>
2021-04-25 17:53:31 +0200

    'Getting started' done

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { logger } from 'redux-logger';
+import promiseMiddleware from 'redux-promise';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+// reducers
+import ChannelsReducer from './reducers/channels_reducer';
+import CurrentUserReducer from './reducers/current_user_reducer';
+import MessagesReducer from './reducers/messages_reducer';
+import SelectedChannelReducer from './reducers/selected_channel_reducer';
+
+
+const initialState = {
+  messages: [],
+  channels: ['general', 'react', 'berlin'],
+  selectedChannel: 'general',
+  currentUser: prompt('your username', 'anonymous')
+  // currentUser: prompt('your username', `anonymous${Math.floor(10 + (Math.rand() * 90))}`)
+};
+
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  // changeMe: (state = null, action) => state
+  messages: MessagesReducer,
+  channels: ChannelsReducer,
+  selectedChannel: SelectedChannelReducer,
+  currentUser: CurrentUserReducer
 });
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, applyMiddleware(logger, promiseMiddleware))}>
     <App />
   </Provider>,
   document.getElementById('root')
